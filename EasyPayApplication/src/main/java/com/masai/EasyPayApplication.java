e4a6fed32123998c330d5726400f5294b16ea085
kPrince114 <princep7277789921@gmail.com>
2023-03-28 14:51:19 +0530

    initial

---
+++ EasyPayApplication/src/main/java/com/masai/EasyPayApplication.java
@@ -0,0 +1,14 @@
+package com.masai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyPayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasyPayApplication.class, args);
+		System.out.println("prince ..........");
+	}
+
+}
