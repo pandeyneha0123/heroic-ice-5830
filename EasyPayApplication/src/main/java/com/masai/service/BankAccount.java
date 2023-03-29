92f5a6430335a1787f4c95d7e00fd678abb1cd91
pandeyneha0123 <pandeyneha0123>
2023-03-29 18:28:27 +0530

    add

---
+++ EasyPayApplication/src/main/java/com/masai/service/BankAccount.java
@@ -0,0 +1,19 @@
+package com.masai.service;
+
+import java.util.List;
+
+import com.masai.model.Customer;
+
+public interface BankAccount {
+	
+	public Customer addAccount(BankAccount Account, String customerId);
+
+	public Customer deleteAccount(Integer BankAccountId, String customerId);
+
+	public BankAccount ViewAccount(String AccountNumber, String customerId);
+
+	public List<BankAccount> ViewAllAccount(Integer wid, String customerId);
+
+	public Customer showBalance(String mobileNumber);
+
+}
