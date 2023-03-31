e640deff49b40f1e0dcda4aa5678ddc6fd94cf21
pandeyneha0123 <pandeyneha0123>
2023-03-31 12:27:01 +0530

    add

--- EasyPayApplication/src/main/java/com/masai/repository/CustomerSessionDao.java
+++ EasyPayApplication/src/main/java/com/masai/repository/CustomerSessionDao.java
@@ -1,5 +1,9 @@
 package com.masai.repository;
 
-public interface CustomerSessionDao {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
+public interface CustomerSessionDao extends JpaRepository<CustomerSession, Integer> {
+	public CustomerSession findByUniqueId(String customerId);
 }
